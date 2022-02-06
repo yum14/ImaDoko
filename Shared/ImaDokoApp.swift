4a84e6647021ca85f8a08fd705514c579bd36e6e
yum14 <yumurata1214@gmail.com>
2022-02-06 14:41:55 +0900

    :tada: Initial commit

---
+++ Shared/ImaDokoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImaDokoApp.swift
+//  Shared
+//
+//  Created by yum on 2022/02/06.
+//
+
+import SwiftUI
+
+@main
+struct ImaDokoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
